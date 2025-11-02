2a4aef42d88e3cb6308e38c37b2567b3b91db7e0
cpowelll <2cedarsabby@gmail.com>
2025-11-01 22:27:16 -0700

    part 5

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+/**
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -7,4 +8,17 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+)*/
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { Authenticator } from "@aws-amplify/ui-react";
+ReactDOM.createRoot(document.getElementById("root")).render(
+<React.StrictMode>
+<Authenticator>
+<App />
+</Authenticator>
+</React.StrictMode>
+);
